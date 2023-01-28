b579c10855c7e71c4b4496f9f0a3be55ed1c515f
Abizeyimana Victor <svicky.shema@gmail.com>
2023-01-28 10:09:01 +0200

    Reposnive & Kinyarwanda

--- portal/src/App.jsx
+++ portal/src/App.jsx
@@ -1,42 +1,38 @@
-import './App.css';
 import { FrappeProvider } from 'frappe-react-sdk';
+
 function App() {
 	return (
-		<div className="App">
+		<div className="bg-mgreen flex flex-wrap mb-4 min-h-screen absolute inset-0">
 			<FrappeProvider>
-				<div className="bg-mgreen h-screen w-screen grid md:grid-cols-2 gap-4">
-					<div className=" w-full h-full flex items-center justify-center p-10">
-						<div className="flex flex-col w-full items-center justify-center text-center">
-							<p className=" text-3xl mb-5 text-center text-white">
-								This Website is under maintenance, <br></br>We
-								will back soon
-							</p>
-							<img
-								className="w-80"
-								src="/assets/portal_mvp/portal/msacco_logo.png"
-							/>
-						</div>
+				<div className=" w-full sm:w-1/2 flex items-center justify-center p-10">
+					<div className="flex flex-col w-full items-center justify-center text-center">
+						<p className=" text-2xl mb-5 text-center text-white">
+							Iyi sisteme iri kuvugururwa <br></br>turayisubizaho
+							mu giho gito kiri imbere.
+						</p>
+						<img
+							className="w-1/2"
+							src="/assets/portal_mvp/portal/msacco_logo.png"
+						/>
 					</div>
-					<div className="w-full h-full flex items-center justify-center">
-						<div className="flex p-12 flex-col items-center justify-center bg-white rounded-3xl w-2/3 h-1/3 text-center">
-							Please use the following email in the meantime
-							<div className="bg-mgreen m-2 rounded-lg text-white w-full p-4">
-								<a
-									href="mailto:loans.application@mugangasacco.rw"
-									className=" font-bold"
-								>
-									Loan: loans.application@mugangasacco.rw
-								</a>
-							</div>
-							<div className="bg-morange m-2 rounded-lg text-white w-full p-4">
-								<a
-									href="mailto:loans.application@mugangasacco.rw"
-									className=" font-bold"
-								>
-									Other: info@mugangasacco.rw
-								</a>
-							</div>
-						</div>
+				</div>
+				<div className="w-full sm:w-1/2 flex items-center justify-center p-3">
+					<div className="flex px-7 py-14 flex-col items-center justify-center bg-white rounded-xl w-full sm:w-3/4  text-center">
+						<button class="btn btn-green w-full mb-4">
+							Saba Inguzanyo kuri:{' '}
+							<strong>loan.application@mugangasacco.rw</strong>
+						</button>
+						{/* <button class="btn btn-orange w-full">
+							Uzuza Amakuru
+						</button> */}
+						<p className="font-bold text-lg mt-2 text-left text-mgreen">
+							<strong>Ukeneye ubufasha? </strong>
+							<hr />
+							Hamagara: <strong>0788 124 500</strong>
+							<br />
+							Twandikire kuri{' '}
+							<strong>info@mugangasacco.rw</strong>
+						</p>
 					</div>
 				</div>
 			</FrappeProvider>
